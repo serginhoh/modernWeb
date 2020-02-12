d8db5f30dc3f87535d3d35803819c14e41082caa
Logon Aluno <logonaluno@local.com>
2020-02-11 21:04:59 -0300

    inicio aula 02

---
+++ Aula02/Desafios/Desafio A/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,12 @@
+class NegociacaoController {
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+    }
+}
